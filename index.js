87c9921143ceb3e87c1759e881b66130f3224766
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-06-02 09:18:23 +0100

    moved index.js: moved file back into root repo

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes/TestRoutes')
+// const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+app.use(router)
+
+// ##########################################################
+// const { Randomize } = require('./hooks/Randomize');
+const path = require('path');
+app.use('/public', express.static(path.join(__dirname, 'hooks', 'public')));
+// ##########################################################
+
+const ash = '#####'
+console.log(
+  '\n', ash.repeat(5),
+  '\nenv:', process.env.NODE_ENV,
+  '\nport:', process.env.PORT,
+  '\nserver origin:', process.env.SERVER_ORIGIN,
+  '\nserver origin from env:', process.env.SERVER_ORIGIN,
+  '\n', ash.repeat(5),
+)
+if (process.env.NODE_ENV !== 'production') {
+  const portNumber = 4000; // 🔢 Port number
+  app.listen(portNumber, () => {
+    console.log(`Server running on http://localhost:${portNumber}`);
+  });
+} else {
+  // vercel deployment
+  module.exports = app;
+}
